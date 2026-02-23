47891ac27e08f99c9886ca03f31727d5e5d17d74
sanchyopas <saniagolovanev@gmail.com>
2026-02-23 19:18:36 +0700

    dev: header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,26 @@
 import * as functions from './modules/baseFunctions.js';
 
 functions.isWebp();
+
+
+const copyPhoneLink = document.querySelectorAll('.phone-copy');
+
+copyPhoneLink.forEach(link => {
+  const tooltip = link.querySelector('.tooltip');
+  link.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const phone = link.dataset.phone;
+    try {
+      await navigator.clipboard.writeText(phone);
+
+      tooltip.classList.add('active');
+
+      setTimeout(() => {
+        tooltip.classList.remove('active');
+      }, 2000);
+
+    } catch {
+      console.log('Не удалось скопировать');
+    }
+  });
+});
