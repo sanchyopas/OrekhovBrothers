3ced48fcade0c8bfe4c8c7f056be23bfd9630004
sanchyopas <saniagolovanev@gmail.com>
2026-03-17 19:47:20 +0700

    refactor header/burger menu only css

--- src/js/app.js
+++ src/js/app.js
@@ -18,38 +18,3 @@ document.querySelectorAll('.js-copy-phone')?.forEach(link => {
   });
 });
 
-const headerBurgerBtn = document.querySelector('.header__burger');
-const navMobile = document.getElementById('mobile-menu');
-
-headerBurgerBtn?.addEventListener('click', (e) => {
-  const rect = e.currentTarget.getBoundingClientRect();
-  if(!navMobile.classList.contains('active')) {
-    navMobile.style.left = rect.left + 'px';
-    navMobile.style.top = rect.bottom + 'px';
-
-    navMobile.classList.add('active');
-  }else {
-    navMobile.classList.remove('active');
-  }
-});
-
-
-const btn = document.getElementById('show-more-options')
-const options = document.querySelector('.options__inner')
-
-btn?.addEventListener('click', () => {
-
-  if (options.classList.contains('active')) {
-    options.style.maxHeight = '114px'
-    options.classList.remove('active')
-    btn.innerText = 'Показать все'
-
-  } else {
-    options.style.maxHeight = options.scrollHeight + 'px'
-    options.classList.add('active')
-
-    btn.innerText = 'Свернуть'
-  }
-
-})
-
