f7c569da661e7bd0ad91736457c41cd50338ea33
sanchyopas <saniagolovanev@gmail.com>
2026-04-09 01:09:28 +0700

    dev dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -29,8 +29,6 @@ const btn = document.getElementById('show-more-options')
 const options = document.querySelector('.options__inner')
 
 btn?.addEventListener('click', () => {
-  console.log(options.scrollHeight)
-
   if (options.classList.contains('active')) {
 
     options.style.maxHeight = '114px'
@@ -45,3 +43,43 @@ btn?.addEventListener('click', () => {
 
 })
 
+
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+  const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+  const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+  const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
+  const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
+
+  // Клик по кнопке. Открыть/Закрыть select
+  dropDownBtn.addEventListener('click', function (e) {
+    dropDownList.classList.toggle('dropdown__list--visible');
+    this.classList.add('dropdown__button--active');
+  });
+
+  // Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+  dropDownListItems.forEach(function (listItem) {
+    listItem.addEventListener('click', function (e) {
+      e.stopPropagation();
+      dropDownBtn.innerText = this.innerText;
+      dropDownBtn.focus();
+      dropDownInput.value = this.dataset.value;
+      dropDownList.classList.remove('dropdown__list--visible');
+    });
+  });
+
+  // Клик снаружи дропдауна. Закрыть дропдаун
+  document.addEventListener('click', function (e) {
+    if (e.target !== dropDownBtn) {
+      dropDownBtn.classList.remove('dropdown__button--active');
+      dropDownList.classList.remove('dropdown__list--visible');
+    }
+  });
+
+  // Нажатие на Tab или Escape. Закрыть дропдаун
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Tab' || e.key === 'Escape') {
+      dropDownBtn.classList.remove('dropdown__button--active');
+      dropDownList.classList.remove('dropdown__list--visible');
+    }
+  });
+});
